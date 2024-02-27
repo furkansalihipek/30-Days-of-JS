1db72ed5563f75e479bd8b271540746ce84ef50f
Furkan Salih  İpek <furkan.ipek@192.168.1.100>
2024-02-28 00:32:13 +0300

    commit day1 and day2

---
+++ day1.js
@@ -0,0 +1,25 @@
+// Consola içerisine girilen ögeyi yazdırır.
+ console.log("Heloo Wolrd");
+
+// typeof içerisine girilen değerin türünü verir.
+console.log(typeof 'Asabeneh');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+// Değişken tanımlamak için let (Değeri değiştirilebilir) ya da const (Sabit değer alır değiştirilemez) kullanılır.
+ let firstName = "Furkan";
+ let age = 20;
+ let isMarried = false;
+ let child = null;
+
+ let firstName1 = "Furkan",
+  age1 = 20,
+ isMarried1 = false,
+  child1 = null
+
+ const PI = 3.14;
+ const Gravity = 9.81;
+
+ console.log(firstName, age, isMarried, child);
